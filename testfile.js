3b9b8c4aecde5075f7a062710f4ee877d11d58a5
anbukkarasi29 <anbushan22@gmail.com>
2026-06-24 23:06:39 +0530

    Add console log for user message in testfile.js
    Added a console log message requesting user input.

--- testfile.js
+++ testfile.js
@@ -3,4 +3,5 @@ test("validate facebook Application", async( {page} ) => {
     await page.goto("https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F");
     console.log(" test file moved to github")
     console.log("welcome new branch")
+    console.log("give some msg pls in github ")
 } )
